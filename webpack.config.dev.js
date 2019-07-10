d2c8a2d3e88ae6cc2b80fba309bc63935b2046db
ojolanre <ojocornelius02@gmail.com>
2019-07-10 03:28:33 +0100

    updates

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,26 +1,23 @@
-import webpack from 'webpack';
 import path from 'path';
 
 export default {
+  debug: true,
   devtool: 'inline-source-map',
-  entry: [path.resolve(__dirname, 'src/index')],
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'src'),
     publicPath: '/',
     filename: 'bundle.js'
   },
-  mode: 'development',
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      debug: false,
-      noInfo: true
-    })
-  ],
+  plugins: [],
   module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
     ]
   }
-};
+}
